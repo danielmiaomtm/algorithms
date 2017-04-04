c0b5f0310150bdcf8c51ded456d5df8fd6860140
danielmiaomtm <jeffreymaomtm@gmail.com>
2017-04-03 19:49:54 -0700

    Update 499. The Maze III.java

--- 499. The Maze III.java
+++ 499. The Maze III.java
@@ -1,8 +1,14 @@
-There is a ball in a maze with empty spaces and walls. The ball can go through empty spaces by rolling up (u), down (d), left (l) or right (r), but it won't stop rolling until hitting a wall. When the ball stops, it could choose the next direction. There is also a hole in this maze. The ball will drop into the hole if it rolls on to the hole.
+/*
+There is a ball in a maze with empty spaces and walls. The ball can go through empty spaces by rolling up (u), down (d), left (l) or 
+right (r), but it won't stop rolling until hitting a wall. When the ball stops, it could choose the next direction. There is also a 
+hole in this maze. The ball will drop into the hole if it rolls on to the hole.
+Given the ball position, the hole position and the maze, find out how the ball could drop into the hole by moving the shortest distance.
+The distance is defined by the number of empty spaces traveled by the ball from the start position (excluded) to the hole (included). 
+Output the moving directions by using 'u', 'd', 'l' and 'r'. Since there could be several different shortest ways, you should output 
+the lexicographically smallest way. If the ball cannot reach the hole, output "impossible".
 
-Given the ball position, the hole position and the maze, find out how the ball could drop into the hole by moving the shortest distance. The distance is defined by the number of empty spaces traveled by the ball from the start position (excluded) to the hole (included). Output the moving directions by using 'u', 'd', 'l' and 'r'. Since there could be several different shortest ways, you should output the lexicographically smallest way. If the ball cannot reach the hole, output "impossible".
-
-The maze is represented by a binary 2D array. 1 means the wall and 0 means the empty space. You may assume that the borders of the maze are all walls. The ball and the hole coordinates are represented by row and column indexes.
+The maze is represented by a binary 2D array. 1 means the wall and 0 means the empty space. You may assume that the borders of the maze
+are all walls. The ball and the hole coordinates are represented by row and column indexes.
 
 Example 1
 
@@ -41,10 +47,10 @@ Explanation: The ball cannot reach the hole.
 Note:
 There is only one ball and one hole in the maze.
 Both the ball and hole exist on an empty space, and they will not be at the same position initially.
-The given maze does not contain border (like the red rectangle in the example pictures), but you could assume the border of the maze are all walls.
+The given maze does not contain border (like the red rectangle in the example pictures), but you could assume the border of the maze 
+are all walls.
 The maze contains at least 2 empty spaces, and the width and the height of the maze won't exceed 30.
-Show Tags
-Show Similar Problems
+*/
 
 
 
