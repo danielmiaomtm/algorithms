43d446e76d7d59b1ac5361618864c84ccbbb8974
danielmiaomtm <jeffreymaomtm@gmail.com>
2017-04-05 16:39:19 -0700

    Create 523. Continuous Subarray Sum.java

---
+++ 523. Continuous Subarray Sum.java
@@ -0,0 +1,50 @@
+/*
+
+
+Given a list of non-negative numbers and a target integer k, write a function to check if the array has a continuous subarray of size at least 2 that sums up to the multiple of k, that is, sums up to n*k where n is also an integer.
+
+Example 1:
+Input: [23, 2, 4, 6, 7],  k=6
+Output: True
+Explanation: Because [2, 4] is a continuous subarray of size 2 and sums up to 6.
+Example 2:
+Input: [23, 2, 6, 4, 7],  k=6
+Output: True
+Explanation: Because [23, 2, 6, 4, 7] is an continuous subarray of size 5 and sums up to 42.
+Note:
+The length of the array won't exceed 10,000.
+You may assume the sum of all the numbers is in the range of a signed 32-bit integer.
+
+
+
+
+Java O(n) time O(k) space
+
+
+Explain idea:
+This is one of those magics of reminder theorem :)
+
+(a+(n*x))%x is same as (a%x)
+
+For e.g. in case of the array [23,2,6,4,7] the running sum is [23,25,31,35,42] and the reminders are [5,1,1,5,0]. 
+We got reminder 5 at index 0 and at index 3. That means, in between these two indexes we must have added a number which is multiple of 
+the k. Hope this clarifies your doubt :)
+*/
+
+
+
+
+public boolean checkSubarraySum(int[] nums, int k) {
+    Map<Integer, Integer> map = new HashMap<Integer, Integer>(){{put(0,-1);}};;
+    int runningSum = 0;
+    for (int i=0;i<nums.length;i++) {
+        runningSum += nums[i];
+        if (k != 0) runningSum %= k; 
+        Integer prev = map.get(runningSum);
+        if (prev != null) {
+            if (i - prev > 1) return true;
+        }
+        else map.put(runningSum, i);
+    }
+    return false;
+}
