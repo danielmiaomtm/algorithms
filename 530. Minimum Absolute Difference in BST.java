c5b3d2f19b574738ddb4dd8e44a184698bbfa16b
danielmiaomtm <jeffreymaomtm@gmail.com>
2017-04-05 17:33:37 -0700

    Create 530. Minimum Absolute Difference in BST.java

---
+++ 530. Minimum Absolute Difference in BST.java
@@ -0,0 +1,56 @@
+/*
+Given a binary search tree with non-negative values, find the minimum absolute difference between values of any two nodes.
+
+Example:
+
+Input:
+
+   1
+    \
+     3
+    /
+   2
+
+Output:
+1
+
+Explanation:
+The minimum absolute difference is 1, which is the difference between 2 and 1 (or between 2 and 3).
+Note: There are at least two nodes in this BST.
+*/
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    TreeSet<Integer> set = new TreeSet<>();
+    int min = Integer.MAX_VALUE;
+    
+    public int getMinimumDifference(TreeNode root) {
+        if (root == null) return min;
+        
+        if (!set.isEmpty()) {
+            if (set.floor(root.val) != null) {
+                min = Math.min(min, root.val - set.floor(root.val));
+            }
+            if (set.ceiling(root.val) != null) {
+                min = Math.min(min, set.ceiling(root.val) - root.val);
+            }
+        }
+        
+        set.add(root.val);
+        
+        getMinimumDifference(root.left);
+        getMinimumDifference(root.right);
+        
+        return min;
+    }
+}
